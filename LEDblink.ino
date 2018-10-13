7be0545bcaad1cb605a26ab778d3983f44303e91
Makani Ramakrishna <32337366+mark9599@users.noreply.github.com>
2018-10-13 22:57:19 +0530

    Add files via upload

---
+++ LEDblink.ino
@@ -0,0 +1,17 @@
+            int p;
+            void setup() {
+         // put your setup code here, to run once:
+                              p=13;
+                              pinMode(p,OUTPUT);
+                              Serial.begin(9600);
+                          }
+
+            void loop() {
+            // put your main code here, to run repeatedly:
+                          digitalWrite(p,HIGH);
+                          delay(1000);
+                          digitalWrite(p,LOW);
+                          delay(1000);
+                          Serial.print("hi");
+                          delay(1000);
+                }
